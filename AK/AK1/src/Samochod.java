7cdbb8267edefdb476ec6660678da7f36c8e00f8
arkowalczyk <arkowalczyk@b11c3a88-3007-4bd8-a6c1-73b3532edbb5>
2016-11-03 10:40:49 +0000

    dodaj Samochod

---
+++ AK/AK1/src/Samochod.java
@@ -0,0 +1,56 @@
+import java.lang.*;
+
+public class Samochod {
+    private String kolor;
+    private String nrRejest;
+    private String model;
+    private String marka;
+    private double predkoscMax;
+
+    private Silnik sil;
+    private SkrzyniaBiegow skrzynia;
+    private Kolo kola;
+
+    public Samochod(String kolor, String nrRejest, String model, String marka, double predkoscMax, Silnik sil, SkrzyniaBiegow skrzynia, Kolo kola) {
+        this.kolor = kolor;
+        this.nrRejest = nrRejest;
+        this.model = model;
+        this.marka = marka;
+        this.predkoscMax = predkoscMax;
+        this.sil = sil;
+        this.skrzynia = skrzynia;
+        this.kola = kola;
+    }
+
+    public void wlacz() {
+
+    }
+
+    public void wylacz() {
+
+    }
+
+    public void jazda() {
+
+    }
+
+    public void obliczWage() {
+
+    }
+
+    public void aktualnaPredkosc() {
+
+    }
+
+    /*public static void main(String[] args) {
+        Samochod s = new Samochod("srebrny", "KR0001", "punto", "fiat", 190,
+                new Silnik("silnik", 250, 1500, "fiat", 6000),
+                new SkrzyniaBiegow("skrzynia", 100, 2500, "fiat", 7),
+                new Kolo("koło", 10.0, 400.0, "Pirelli", 24));
+
+        Samochod s2 = new Samochod("biały", "KRA007", "vectra", "opel", 260,
+                new Silnik("silnik2", 300, 3000, "opel", 7000),
+                new SkrzyniaBiegow("skrzynia2", 150, 4000, "opel", 7),
+                new Kolo("koło2", 12, 500, "Debica", 26));
+    }*/
+}
---
+++ AK/AK1/src/Silnik.java
@@ -0,0 +1,41 @@
+public class Silnik extends Komponent {
+    private double maxObroty;
+    private double obroty = 0;
+
+    public Silnik(String nazwa, double waga, double cena, String producent, double maxObroty) {
+        super(nazwa, waga, cena, producent);
+        this.maxObroty = maxObroty;
+    }
+
+    public void uruchom() {
+        obroty = 800; //bieg jałowy
+        System.out.println("silnik uruchomiony.");
+        System.out.println("obroty = " + obroty);
+    }
+
+    public void zatrzymaj() {
+        obroty = 0; //stop
+        System.out.println("silnik zatrzymany.");
+        System.out.println("obroty = " + obroty);
+    }
+
+    public void zwiekszObroty() {
+        obroty = obroty + 100;
+        if (obroty > maxObroty) obroty = maxObroty;
+        System.out.println("obroty = " + obroty
+        );
+    }
+
+    public void zmniejszObroty() {
+        obroty = obroty - 100;
+        if (obroty < 800) obroty = 800;
+        System.out.println("obroty = " + obroty);
+    }
+
+    /*public static void main(String[] args) {
+        Silnik s = new Silnik();
+        s.uruchom();
+        s.zwiekszObroty();
+        s.zatrzymaj();
+    }*/
+}
