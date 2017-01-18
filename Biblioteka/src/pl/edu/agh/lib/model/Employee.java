d0cd73d9032e4470295de4c5d490d2c70daf117b
arkowalczyk <arkowalczyk@b11c3a88-3007-4bd8-a6c1-73b3532edbb5>
2017-01-18 22:18:04 +0000

    final version without UML diagram

--- Biblioteka/src/pl/edu/agh/lib/model/Employee.java
+++ Biblioteka/src/pl/edu/agh/lib/model/Employee.java
@@ -4,5 +4,35 @@ package pl.edu.agh.lib.model;
  * Created by Arek on 2017-01-11.
  */
 public class Employee {
+    private String login;
+    private String password;
 
+    public Employee(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return login + " " + password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (!login.equals(employee.login)) return false;
+        return password.equals(employee.password);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
 }
